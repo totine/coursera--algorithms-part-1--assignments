e7dd072162b9a8cfb1675ee73cb6564f474122e7
Joanna <joanna.gargas@gmail.com>
2018-01-21 19:00:36 +0100

    Add Node constructor and Deque size, isEmpty, addFirst, addLast methods

--- week-2-assignment--deques-and-randomized-queues/src/Deque.java
+++ week-2-assignment--deques-and-randomized-queues/src/Deque.java
@@ -3,12 +3,19 @@ import java.util.Iterator;
 public class Deque<Item> implements Iterable<Item> {
     private Node first;
     private Node last;
+    private int size;
 
 
     private class Node {
         Item value;
         Node next;
         Node previous;
+
+        public Node(Item item) {
+            value = item;
+            next = null;
+            previous = null;
+        }
     }
     /**
      * construct an empty deque
@@ -16,34 +23,55 @@ public class Deque<Item> implements Iterable<Item> {
     public Deque() {
         first = null;
         last = null;
+        size = 0;
     }
 
     /**
      * is the deque empty?
      */
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     /**
      * return the number of items on the deque
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
      * add the item to the front
      */
     public void addFirst(Item item) {
+         Node node = new Node(item);
+         if (first == null && last == null) {
+             first = node;
+             last = node;
+         }
 
+         else {
+             node.next = first;
+             first = node;
+         }
+         size++;
     }
 
     /**
      * add the item to the end
      */
     public void addLast(Item item) {
+        Node node = new Node(item);
+        if (first == null && last == null) {
+            first = node;
+            last = node;
+        }
 
+        else {
+            last.next = node;
+            last = node;
+        }
+        size++;
     }
 
     /**
