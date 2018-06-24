f0f514663114cedcd8bc2d8eff2148c6441ab19d
Joanna <joanna.gargas@gmail.com>
2018-06-24 17:27:59 +0200

    Add empty methods to RandomizedQueue

--- week-2-assignment--deques-and-randomized-queues/src/RandomizedQueue.java
+++ week-2-assignment--deques-and-randomized-queues/src/RandomizedQueue.java
@@ -1,2 +1,54 @@
-public class RandomizedQueue {
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    /**
+     * construct an empty randomized queue* @return
+     */
+    public RandomizedQueue() {
+    }
+
+    /**
+     * is the randomized queue empty?
+     */
+    public boolean isEmpty() {
+        return false;
+    }                 //
+
+    /**
+     * @return the number of items on the randomized queue
+     */
+    public int size() {
+        return 0;
+    }                        // return
+
+    /**
+     * add the item
+     *
+     * @param item to add
+     */
+    public void enqueue(Item item) {
+    }           //
+
+    /**
+     * remove and return a random item
+     *
+     * @return a random item
+     */
+    public Item dequeue() {
+        return null;
+    }
+
+    /**
+     * @return a random item (but do not remove it)
+     */
+    public Item sample() {
+        return null;
+    }
+
+    /**
+     * @return an independent iterator over items in random order
+     */
+    public Iterator<Item> iterator() {
+        return null;
+    }
 }
